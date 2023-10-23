00e8dfeaf55f0c36bff202142ade2828cf81869a
i12cu84 <49804511+i12cu84@users.noreply.github.com>
2023-10-23 14:57:03 +0800

    2023.10.23

--- CppPeculiarity/cpp11/nullptr.cpp
+++ CppPeculiarity/cpp11/nullptr.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 using namespace std;
 void func(void *ptr) 
@@ -16,5 +15,3 @@ int main() {
     func(nullptr); // 输出func ptr
     return 0;
 }
-```
-nullptr是用来表示空指针新引入的常量值，在c++中如果表示空指针语义时建议使用nullptr而不要使用NULL，因为NULL本质上是个int型的0，其实不是个指针。
