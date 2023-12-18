cb911c86e9f5f38a8bbd1539a7fe3d059f791116
i12cu84 <49804511+i12cu84@users.noreply.github.com>
2023-12-18 09:23:43 +0800

    2023.12.18

---
+++ Tools/UE/code/Engine/UnrealEngine.cpp
@@ -0,0 +1,3 @@
+//C:\Program Files\Epic Games\UE_5.2\Engine\Source\Runtime\Engine\Private\UnrealEngine.cpp
+
+//实现UEngine类和辅助方法。
---
+++ Tools/UE/code/UObject/PackedObjectRef.h
@@ -0,0 +1,10 @@
+//
+
+namespace UE::CoreUObject::Private
+{
+    /* *
+    * FPackedObjectRef表示一个轻量级的引用，可以装入指针空间并能够引用对象
+    *(或null)可以加载也可以不加载，而不指向其在内存中的位置(即使它当前已经加载)。
+    */
+    struct FPackedObjectRef{};
+}
