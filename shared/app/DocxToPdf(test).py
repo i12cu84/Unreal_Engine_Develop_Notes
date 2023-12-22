a80fccaa0e1cf12b61f7b53d7628817862d0653e
i12cu84 <49804511+i12cu84@users.noreply.github.com>
2023-12-22 09:19:10 +0800

    2023.12.22

---
+++ shared/app/DocxToPdf(test).py
@@ -0,0 +1,54 @@
+#Word转Pdf (coding) 有折损版
+from docx import Document
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+import os
+
+# 获取当前工作目录
+current_directory = os.getcwd()
+
+# 字体文件的路径
+font_path = os.path.join(current_directory, 'yahei.ttf')
+
+# 注册字体
+pdfmetrics.registerFont(TTFont('YaHei', font_path))
+
+def docx_to_pdf(docx_path, pdf_path):
+    # 加载文档
+    doc = Document(docx_path)
+    pdf_file = open(pdf_path, "wb")
+
+    # 创建PDF和画布
+    c = canvas.Canvas(pdf_file, pagesize=letter)
+    width, height = letter  # 获取letter纸张大小
+
+    # 遍历文档中的每一段，写入PDF
+    for para in doc.paragraphs:
+        # 使用注册的字体
+        c.setFont('YaHei', 10)  # 这里使用注册的字体
+        c.drawString(100, height - 100, para.text)
+        height -= 20  # 下移一段的距离
+
+    # 保存PDF
+    c.save()
+    pdf_file.close()
+
+def convert_docs_to_pdf(directory):
+    # 遍历目录中的所有文件
+    for filename in os.listdir(directory):
+        if filename.endswith(".docx"):
+            # 构建完整的文件路径
+            docx_path = os.path.join(directory, filename)
+            # 构建PDF的文件路径
+            pdf_path = os.path.join(directory, filename.replace(".docx", ".pdf"))
+            # 调用转换函数
+            print(f'Converting {filename} to {filename.replace(".docx", ".pdf")}')
+
+            docx_to_pdf(docx_path, pdf_path)
+            print(f"Converted {filename} to {filename.replace('.docx', '.pdf')}")
+
+# 指定要转换的目录
+directory = 'C:/Users/i12cu84/ocr/'
+convert_docs_to_pdf(directory)
--- shared/app/EpubToPdf.py
+++ shared/app/EpubToPdf.py
@@ -1,1 +1,28 @@
 #epub转pdf
+import os
+import pypandoc
+
+def convert_epub_to_pdf(epub_path, pdf_path):
+    # 创建PDF文件
+    pdf_filename = os.path.splitext(os.path.basename(epub_path))[0] + ".pdf"
+    pdf_file_path = os.path.join(pdf_path, pdf_filename)
+
+    # 使用pandoc将EPUB转换为PDF，选择xelatex引擎，并指定字体
+    extra_args = ['--pdf-engine=xelatex', '--pdf-engine-opt=-8bit', '--variable', 'mainfont=NotoSerifCJKsc-Regular.otf', '--dpi=300']
+    pypandoc.convert_file(epub_path, "pdf", outputfile=pdf_file_path, extra_args=extra_args)
+
+    print(f"转换完成: {pdf_file_path}")
+
+def convert_epubs_to_pdfs(epub_folder, pdf_folder):
+    # 确保PDF文件夹存在
+    if not os.path.exists(pdf_folder):
+        os.makedirs(pdf_folder)
+
+    # 遍历epub文件夹
+    for root, dirs, files in os.walk(epub_folder):
+        for file in files:
+            if file.endswith(".epub"):
+                epub_path = os.path.join(root, file)
+                convert_epub_to_pdf(epub_path, pdf_folder)
+convert_epubs_to_pdfs("C:/Users/i12cu84/Desktop/tianya/more/epub", "C:/Users/i12cu84/Desktop/tianya/more/epub")
+
